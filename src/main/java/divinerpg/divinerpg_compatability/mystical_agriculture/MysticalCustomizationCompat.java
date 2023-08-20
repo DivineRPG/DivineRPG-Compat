417a3cf27b96a513f357e9d885049791807cf0d4
Nicusha <nicosaurusrex99@gmail.com>
2023-08-21 03:52:19 +0930

    improved frame for mystical customization

--- src/main/java/divinerpg/divinerpg_compatability/mystical_agriculture/MysticalCustomizationCompat.java
+++ src/main/java/divinerpg/divinerpg_compatability/mystical_agriculture/MysticalCustomizationCompat.java
@@ -3,11 +3,42 @@ package divinerpg.divinerpg_compatability.mystical_agriculture;
 import divinerpg.DivineRPG;
 
 import java.io.*;
+import java.util.*;
 
 public class MysticalCustomizationCompat {
     public static void init() {
-        // Define the JSON content
-        String apalachiaFragments = "{\n" +
+
+        Map<String, String> jsonContentMap = new HashMap<>();
+        jsonContentMap.put("apalachia_fragments.json", generateApalachiaFragmentsJSON());
+
+        // Define the base directory for config files
+        String configDir = "config/mysticalcustomization/crops/";
+
+        // Loop through the filenames and write the JSON content to each file
+        for (String fileName : jsonContentMap.keySet()) {
+            File configFile = new File(configDir + fileName);
+
+            // Ensure parent directories exist
+            File parentDir = configFile.getParentFile();
+            if (!parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+
+            if (!configFile.exists()) {
+                String jsonContent = jsonContentMap.get(fileName);
+                try (Writer output = new BufferedWriter(new FileWriter(configFile))) {
+                    output.write(jsonContent);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    // Define a method to generate JSON content
+    private static String generateApalachiaFragmentsJSON() {
+        return "{\n" +
                 "    \"name\": \"Apalachia Fragments\",\n" +
                 "    \"type\": \"mysticalagriculture:resource\",\n" +
                 "    \"tier\": \"mysticalagriculture:6\",\n" +
@@ -23,33 +54,5 @@ public class MysticalCustomizationCompat {
                 "    \"baseSecondaryChance\": 0.4,\n" +
                 "    \"crux\": \"divinerpg:apalachia_block\"\n" +
                 "}";
-
-        // Define a list of filenames to be written
-        String[] fileNames = {
-                "apalachia_fragments.json",
-        };
-
-        // Define the base directory for config files
-        String configDir = "config/mysticalcustomization/crops/";
-
-        // Loop through the filenames and write the JSON content to each file
-        for (String fileName : fileNames) {
-            File configFile = new File(configDir + fileName);
-
-            // Ensure parent directories exist
-            File parentDir = configFile.getParentFile();
-            if (!parentDir.exists()) {
-                parentDir.mkdirs();
-            }
-
-            if (!configFile.exists()) {
-                try (Writer output = new BufferedWriter(new FileWriter(configFile))) {
-                    output.write(apalachiaFragments);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    throw new RuntimeException(e);
-                }
-            }
-        }
     }
 }
