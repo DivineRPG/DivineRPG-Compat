7c3bba9515dca02f0663f5f619a6cdc4eb97c92a
Lemon-Juiced <tbrotheawesome@gmail.com>
2023-09-03 00:36:14 -0400

    Changed up the need for cruxes on the non-twilight resources.

--- src/main/java/divinerpg/divinerpg_compatability/mystical_agriculture/MysticalCustomizationCompat.java
+++ src/main/java/divinerpg/divinerpg_compatability/mystical_agriculture/MysticalCustomizationCompat.java
@@ -15,13 +15,11 @@ public class MysticalCustomizationCompat {
         jsonContentMap.put("skythern_fragments.json", generateJSON("Skythern Fragments", 4, "divinerpg:skythern_fragments", "D6D6D1", "mysticalagriculture:block/flower_gem", "mysticalagriculture:item/essence_gem", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:skythern_block"));
         jsonContentMap.put("mortum_fragments.json", generateJSON("Mortum Fragments", 5, "divinerpg:mortum_fragments", "251E1E", "mysticalagriculture:block/flower_gem", "mysticalagriculture:item/essence_gem", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:mortum_block"));
 
-        //TODO - Arlemite, Bloodgem, Realmite, Rupee and Torridite ingot mystical agriculture compat (Completed) - Just waiting for the okay
-        //TODO - Determine if these need a crux, we can likely override the method without a crux
-        jsonContentMap.put("arlemite_ingot.json", generateJSON("Arlemite Ingot", 1, "divinerpg:arlemite_ingot", "9DD75A", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:arlemite_block"));
-        jsonContentMap.put("bloodgem.json", generateJSON("Bloodgem", 1, "divinerpg:bloodgem", "E76587", "mysticalagriculture:block/flower_gem", "mysticalagriculture:item/essence_gem", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:bloodgem_block"));
-        jsonContentMap.put("realmite_ingot.json", generateJSON("Realmite Ingot", 1, "divinerpg:realmite_ingot", "E48E2A", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:realmite_block"));
-        jsonContentMap.put("rupee_ingot.json", generateJSON("Rupee Ingot", 1, "divinerpg:rupee_ingot", "3BC6E4", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:rupee_block"));
-        jsonContentMap.put("torridite_ingot.json", generateJSON("Torridite Ingot", 1, "divinerpg:torridite_ingot", "F45321", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4, "divinerpg:torridite_block"));
+        jsonContentMap.put("arlemite_ingot.json", generateJSON("Arlemite Ingot", 1, "divinerpg:arlemite_ingot", "9DD75A", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4));
+        jsonContentMap.put("bloodgem.json", generateJSON("Bloodgem", 1, "divinerpg:bloodgem", "E76587", "mysticalagriculture:block/flower_gem", "mysticalagriculture:item/essence_gem", "mysticalagriculture:item/mystical_seeds", 0.4));
+        jsonContentMap.put("realmite_ingot.json", generateJSON("Realmite Ingot", 1, "divinerpg:realmite_ingot", "E48E2A", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4));
+        jsonContentMap.put("rupee_ingot.json", generateJSON("Rupee Ingot", 1, "divinerpg:rupee_ingot", "3BC6E4", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4));
+        jsonContentMap.put("torridite_ingot.json", generateJSON("Torridite Ingot", 1, "divinerpg:torridite_ingot", "F45321", "mysticalagriculture:block/flower_ingot", "mysticalagriculture:item/essence_ingot", "mysticalagriculture:item/mystical_seeds", 0.4));
 
         // Define the base directory for config files
         String configDir = "config/mysticalcustomization/crops/";
@@ -48,7 +46,20 @@ public class MysticalCustomizationCompat {
         }
     }
 
-    // Define a method to generate JSON content
+    /**
+     * Defines a method to generate JSON content (with a crux)
+     *
+     * @param name The name of the material
+     * @param tier The tier of the material
+     * @param item The item that represents the material
+     * @param color The color of the material
+     * @param flowerTexture The texture of the flower for the material
+     * @param essenceTexture The texture of the essence for the material
+     * @param seedsTexture The texture of the seeds for the material
+     * @param baseSecondaryChance The chance for a secondary drop for the material
+     * @param crux The crux for the material
+     * @return A String representation of the JSON file
+     */
     private static String generateJSON(String name, int tier, String item, String color, String flowerTexture, String essenceTexture, String seedsTexture, double baseSecondaryChance, String crux) {
         JsonObject json = new JsonObject();
         json.addProperty("name", name);
@@ -69,6 +80,42 @@ public class MysticalCustomizationCompat {
 
         json.addProperty("baseSecondaryChance", baseSecondaryChance);
         json.addProperty("crux", crux);
+
+        return json.toString();
+    }
+
+    /**
+     * Defines a method to generate JSON content (without a crux)
+     *
+     * @param name The name of the material
+     * @param tier The tier of the material
+     * @param item The item that represents the material
+     * @param color The color of the material
+     * @param flowerTexture The texture of the flower for the material
+     * @param essenceTexture The texture of the essence for the material
+     * @param seedsTexture The texture of the seeds for the material
+     * @param baseSecondaryChance The chance for a secondary drop for the material
+     * @return A String representation of the JSON file
+     */
+    private static String generateJSON(String name, int tier, String item, String color, String flowerTexture, String essenceTexture, String seedsTexture, double baseSecondaryChance) {
+        JsonObject json = new JsonObject();
+        json.addProperty("name", name);
+        json.addProperty("type", "mysticalagriculture:resource");
+        json.addProperty("tier", "mysticalagriculture:" + tier);
+
+        JsonObject ingredient = new JsonObject();
+        ingredient.addProperty("item", item);
+        json.add("ingredient", ingredient);
+
+        json.addProperty("color", color);
+
+        JsonObject textures = new JsonObject();
+        textures.addProperty("flower", flowerTexture);
+        textures.addProperty("essence", essenceTexture);
+        textures.addProperty("seeds", seedsTexture);
+        json.add("textures", textures);
+
+        json.addProperty("baseSecondaryChance", baseSecondaryChance);
 
         return json.toString();
     }
